9a06cef17d6fcc3cc914d8f72ae3cbda3d5250a4
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-11-29 16:08:13 -0800

    implemented process_website wheere i am receiving the employee  data and inputting it into

--- pdoc.py
+++ pdoc.py
@@ -10,7 +10,11 @@ from selenium.webdriver.support.ui import Select
 
 
 class EmployeeProcessor:
+    """
+    Reads file with Pandas
 
+    :return employee list
+    """
     def __init__(self, file_name):
         """
         Initialize an OrderProcessor object using a file name, with an empty order list to start.
@@ -22,7 +26,7 @@ class EmployeeProcessor:
 
     def create_employee_list(self):
         """
-        Build and return an employee list. Reads
+        Build and return an employee list
 
         :return: Order list
         """
@@ -43,6 +47,9 @@ class EmployeeProcessor:
 
 
 class Employee:
+    """
+    Employee class with property methods
+    """
     def __init__(self):
         self._employee_name = ""
         self._employer = ""
@@ -91,30 +98,79 @@ class Employee:
         self._vacation_pay = value
 
 
-def start_driver(website):
-    return website
-
-
 def process_website(employees):
-    # get element
+    """
+    Instantiates
+    :param employees: list of employee objects
+    """
+    # Instantiate driver
     driver = webdriver.Firefox()
+    # Get website
     driver.get(
         "https://www.canada.ca/en/revenue-agency/services/e-services/e-services-businesses/payroll-deductions-online"
         "-calculator.html")
-    tax_website = start_driver(driver)
-
+    # Sleep to give time for website to load (pry better way  to check if it's loaded)
+    time.sleep(3)
+    # Assert we're on the correct page
     assert "Payroll Deductions Online Calculator - Canada.ca" in driver.title
+    # Click on initial page " I accept " button
+    initial_accept = driver.find_element(By.PARTIAL_LINK_TEXT, "calculation")
+    initial_accept.click()
+    time.sleep(3)
+    # Next page just need to preess neext
+    next1 = driver.find_element(By.ID, "welcome_button_next")
+    next1.click()
+    time.sleep(3)
+    # jurisdiction/pay period can reside outside of loop
+    # because next calculation button at end select these values again
+    employment_province = Select(driver.find_element(By.ID, "jurisdiction"))
+    employment_province.select_by_visible_text("British Columbia")
+    pay_period = Select(driver.find_element(By.ID, "payPeriodFrequency"))
+    pay_period.select_by_value("SEMI_MONTHLY")
+    # Start processing employees into input page
     for employee in employees:
-        elem = tax_website.find_element(By.PARTIAL_LINK_TEXT, "calculation")
-        elem.click()
-        time.sleep(3)
-        elem2 = tax_website.find_element(By.ID, "welcome_button_next")
-        elem2.click()
-        name_input = tax_website.find_element(By.ID, "employeeName")
+        # Input Name
+        name_input = driver.find_element(By.ID, "employeeName")
         name_input.send_keys(employee.employee_name)
-        employer_name = tax_website.find_element(By.ID, "employerName")
+        # Input Employer
+        employer_name = driver.find_element(By.ID, "employerName")
         employer_name.send_keys(employee.employer)
-    assert "No results found." not in driver.page_source
+        # Extract pandas Timestamp object
+        employee_date = pd.Timestamp.date(employee.date)
+        # Input Date
+        # Year
+        date_paid_year = Select(driver.find_element(By.ID, "datePaidYear"))
+        date_paid_year.select_by_value(str(employee_date.year))
+        # Month
+        date_paid_month = Select(driver.find_element(By.ID, "datePaidMonth"))
+        date_paid_month.select_by_value(str(employee_date.month))
+        # Day
+        date_paid_day = Select(driver.find_element(By.ID, "datePaidDay"))
+        date_paid_day.select_by_value(str(employee_date.day))
+        # Finish form
+        next2 = driver.find_element(By.ID, "payrollDeductionsStep1_button_next")
+        next2.click()
+        time.sleep(3)
+        # Input Salary
+        salary_input = driver.find_element(By.ID, "incomeAmount")
+        salary_input.send_keys(employee.salary)
+        # Input Vacation Pay
+        vp_input = driver.find_element(By.ID, "vacationPay")
+        vp_input.send_keys(employee.vacation_pay)
+        # Finish form
+        next3 = driver.find_element(By.ID, "payrollDeductionsStep2a_button_next")
+        next3.click()
+        # Calculate taxes
+        calculate_button = driver.find_element(By.ID, "payrollDeductionsStep3_button_calculate")
+        calculate_button.click()
+        # Save result
+        save_result = driver.find_element(By.ID, "payrollDeductionsResults_button_generateResultsPdf")
+        save_result.click()
+        # Go to next calculation
+        next_calculation = driver.find_element(By.ID, "payrollDeductionsResults_button_nextCalculationButton")
+        next_calculation.click()
+        time.sleep(2)
+    # Close driver (closes website)
     driver.close()
 
 
